3490fe402a695c1729f9ce2c574663a11f28269b
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2023-12-19 10:01:08 +0900

    [FEAT] - 로그인 remember-me 기능 추가

--- src/main/java/com/windsome/config/SecurityConfig.java
+++ src/main/java/com/windsome/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.windsome.config;
 
+import com.windsome.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,16 +24,15 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     private final AuthenticationFailureHandler customAuthFailureHandler;
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
-    @Bean
-    public PasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
         return (web) -> web.ignoring()
                 .mvcMatchers("/imgs/**")
+                .antMatchers("/h2-console/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
@@ -44,8 +47,7 @@ public class SecurityConfig {
         http
                 .sessionManagement(
                 session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-        );
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
         http.formLogin()
                 .loginPage("/login")
@@ -59,8 +61,20 @@ public class SecurityConfig {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/");
 
+        http
+                .rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(getJDBCRepository());
+
 //        http.csrf().disable();
 
         return http.build();
     }
+
+    private PersistentTokenRepository getJDBCRepository(){
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
 }
+
