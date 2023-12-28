1b3f0d29115067e65b151330b7eb4394d6132003
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2023-12-28 14:49:17 +0900

    [FIX] - Thymeleaf T() 표현식 오류 해결

--- src/main/java/com/windsome/constant/ItemSellStatus.java
+++ src/main/java/com/windsome/constant/ItemSellStatus.java
@@ -1,5 +1,10 @@
 package com.windsome.constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ItemSellStatus {
-    SELL, SOLD_OUT
+    SELL, SOLD_OUT;
 }
