18f017ff5149158da0a618f8010bc5fd290f9cae
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2023-12-29 15:29:08 +0900

    [FEAT] - 아이템 상세 화면 구현

---
+++ src/main/java/com/windsome/controller/ItemController.java
@@ -0,0 +1,23 @@
+package com.windsome.controller;
+
+import com.windsome.dto.ItemFormDto;
+import com.windsome.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId) {
+        ItemFormDto itemFormDto = itemService.getItemFormDto(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
+    }
+}
